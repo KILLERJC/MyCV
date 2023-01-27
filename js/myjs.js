ac0036f5a1224b9059483347502b96fbacaffb8f
Fabian Gregorat <gregoratfabian@gmail.com>
2023-01-27 11:33:20 -0300

    Agregado JS para obtener Usuario Random

---
+++ js/myjs.js
@@ -0,0 +1,39 @@
+/*
+*
+*  Conexion a Random User para cargar los datos en la pagina.
+*  Este se ejecuta apenas carga todo el DOM
+*
+*/
+
+document.addEventListener('DOMContentLoaded', cargarDatos , false);
+
+function cargarDatos(){
+    getJSON('https://randomuser.me/api/',function(err,data){
+        if(err !== null) {
+            /* Algo salio mal, dejamos los valores por defecto*/
+            console.log("No pudimos conectarnos!!");
+        } else {
+            let datos;
+            datos = data.results[0];
+            console.log(datos);
+            document.getElementById('imagenpersona').setAttribute('src',datos.picture.large);
+            document.getElementById('nombrepersona').innerHTML = datos.name.title + " " + datos.name.first + " " + datos.name.last;
+            document.getElementById('resumenpersona').innerHTML = "Tengo " + datos.dob.age + " años y vivo en " + datos.location.city + ", " + datos.location.state + ", " + datos.location.country + ". " + document.getElementById('resumenpersona').innerHTML;
+        }
+    });
+}
+
+let getJSON = function(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      let status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
